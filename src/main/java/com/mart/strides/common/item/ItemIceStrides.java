93dd58d3efcd3e1ac7f39996fb31599752701327
Unknown <mcpkmart@hotmail.nl>
2018-04-03 11:39:26 +0200

    Ice improve

--- src/main/java/com/mart/strides/common/item/ItemIceStrides.java
+++ src/main/java/com/mart/strides/common/item/ItemIceStrides.java
@@ -3,6 +3,7 @@ package com.mart.strides.common.item;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.world.World;
 
 public class ItemIceStrides extends ItemArmorBase {
@@ -15,6 +16,31 @@ public class ItemIceStrides extends ItemArmorBase {
     public void onArmorTick(World world, EntityPlayer player, ItemStack itemStack) {
         if(player.getEntityWorld().getBlockState(player.getPosition().add(0, -1, 0)).getBlock() == Blocks.WATER){
             player.getEntityWorld().setBlockState(player.getPosition().add(0, -1, 0), Blocks.ICE.getDefaultState());
+        }
+
+        EnumFacing facing = player.getHorizontalFacing();
+
+        switch(facing){
+            case EAST:
+                if(player.getEntityWorld().getBlockState(player.getPosition().add(1, -1, 0)).getBlock() == Blocks.WATER){
+                    player.getEntityWorld().setBlockState(player.getPosition().add(1, -1, 0), Blocks.ICE.getDefaultState());
+                }
+                break;
+            case WEST:
+                if(player.getEntityWorld().getBlockState(player.getPosition().add(-1, -1, 0)).getBlock() == Blocks.WATER){
+                    player.getEntityWorld().setBlockState(player.getPosition().add(-1, -1, 0), Blocks.ICE.getDefaultState());
+                }
+                break;
+            case NORTH:
+                if(player.getEntityWorld().getBlockState(player.getPosition().add(0, -1, -1)).getBlock() == Blocks.WATER){
+                    player.getEntityWorld().setBlockState(player.getPosition().add(0, -1, -1), Blocks.ICE.getDefaultState());
+                }
+                break;
+            case SOUTH:
+                if(player.getEntityWorld().getBlockState(player.getPosition().add(0, -1, 1)).getBlock() == Blocks.WATER){
+                    player.getEntityWorld().setBlockState(player.getPosition().add(0, -1, 1), Blocks.ICE.getDefaultState());
+                }
+                break;
         }
     }
 }
